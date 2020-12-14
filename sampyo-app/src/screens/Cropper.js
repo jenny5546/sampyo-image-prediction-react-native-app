dcdd51c06ceae8d1f173952e46c051fc8c1c80dc
Jenny Lee <jenny5546@naver.com>
2020-12-14 11:27:15 +0900

    loader size + app name fix

--- sampyo-app/src/screens/Cropper.js
+++ sampyo-app/src/screens/Cropper.js
@@ -247,8 +247,8 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     lottie: {
-        width: 150,
-        height: 150,
+        width: 100,
+        height: 100,
         marginTop: -20,
     },
     buttonContainer: {
